91183962222ed750e1be5c2b13ed12449c1a1fc6
Eduardo Alvarado <eduardo@ecolifylat.me>
2025-11-10 01:05:23 -0600

    dm2q: initial commit

---
+++ target/dm2q/patches/stock/customize.sh
@@ -0,0 +1,17 @@
+
+echo "- Adding stock system features"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/permissions/com.sec.feature.cover.clearsideviewcover.xml" 0 0 644 "u:object_r:system_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/permissions/com.sec.feature.pocketmode_level33.xml" 0 0 644 "u:object_r:system_file:s0"
+
+echo "- Adding stock GameDriver"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/priv-app/GameDriver-sm8550/GameDriver-sm8550.apk" 0 0 644 "u:object_r:system_file:s0"
+
+echo "- Fixing artifacting and WiFi"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/bin/insthk"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/bin/remotedisplay"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libhwui.so"
+
+echo "- Adding stock WFD blobs"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/bin/insthk" 0 2000 755 "u:object_r:insthk_exec:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/bin/remotedisplay" 0 2000 755 "u:object_r:remotedisplay_exec:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libremotedisplay.so" 0 0 644 "u:object_r:system_lib_file:s0"
