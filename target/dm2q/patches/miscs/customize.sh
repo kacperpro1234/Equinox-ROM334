2ce0bb36ae0cd802a45dda35d91d3793e0a3d472
Eduardo <eduardo@ecolifylat.me>
2025-11-12 00:34:21 -0600

    Update fingerprint sensor configuration and adjust display settings

--- target/dm2q/patches/miscs/customize.sh
+++ target/dm2q/patches/miscs/customize.sh
@@ -8,8 +8,8 @@ sed -i "/# Removed by /d" "$WORK_DIR/product/etc/build.prop" \
 echo "- Enabling adaptive FPS"
 SET_PROP "vendor" "ro.surface_flinger.use_content_detection_for_refresh_rate" "true"
 echo "- Fixing DPI"
-SET_PROP "vendor" "ro.sf.init.lcd_density" "480"
+SET_PROP "vendor" "ro.sf.init.lcd_density" "450"
 sed -i \
-    "/use_content_detection/a ro.surface_flinger.set_idle_timer_ms=3000\nro.surface_flinger.set_touch_timer_ms=500\nro.surface_flinger.set_display_power_timer_ms=1000" \
+    "/use_content_detection/a ro.surface_flinger.set_idle_timer_ms=250\nro.surface_flinger.set_touch_timer_ms=300" \
     "$WORK_DIR/vendor/default.prop"
 
